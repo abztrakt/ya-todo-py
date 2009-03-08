eda8ab3cea31bd1bea43759b50839965f5f5c8d6
Craig <cstimmel@walnut.eplt.washington.edu>
2009-03-08 10:09:57 -0700

    * Parse function opens a tempfile for writing, writes the resource that was originally passed to chooser.py, opens the tempfile in vim, allows for editing, and then reads the re-saved tempfile. In editing, todos should be edited to be things on newlines.

--- chooser.py
+++ chooser.py
@@ -4,22 +4,37 @@ chooser.py - a parsescript for todo.py
     Takes in a file path or URL.
 """
 
-__date__    = "2009/03/05"
 __author__  = "Craig M. Stimmel"
 __license__ = "GPL"
 
 import sys
+import subprocess
 import getopt
 import openanything
+
+EDITOR = "vim"
 
 def getFile(filepath):
     """ Use openanything to open a file or url and return a dictionary of info about it """
     file = openanything.fetch(filepath)
     return file
 
-def parseFile():
+def parseFile(file):
     """ Process the text and return todo items that can be written to the todo.txt data file """
-    pass
+    # Create a tempfile and write our data into it
+    tempfile = "/tmp/todotemp.txt"
+    thandle = open(tempfile, 'w')
+    thandle.write(file['data'])
+    thandle.close()
+
+    # Open our tempfile and edit it
+    edit = subprocess.call([EDITOR, tempfile])
+
+    # Read the tempfile back in as todos
+    thandle = open(tempfile, 'r')
+    todos = thandle.read()
+    thandle.close()
+    return todos
 
 def writeTodos():
     """ Write out the todo items into the todo.txt storage """
@@ -27,7 +42,7 @@ def writeTodos():
 
 def main(filepath):
     file = getFile(filepath)
-    print file["data"]
+    print parseFile(file)
 
 if __name__ == "__main__":
     filepath = sys.argv[1]
